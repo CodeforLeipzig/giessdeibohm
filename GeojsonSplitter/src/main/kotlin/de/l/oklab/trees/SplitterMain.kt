a190eeddb2fdfd234786162248d85445165159b0
Jörg Reichert <de.abg.reichert.joerg@googlemail.com>
2023-01-21 20:35:12 +0100

    Update 2023-01-20

--- GeojsonSplitter/src/main/kotlin/de/l/oklab/trees/SplitterMain.kt
+++ GeojsonSplitter/src/main/kotlin/de/l/oklab/trees/SplitterMain.kt
@@ -13,7 +13,7 @@ sealed class Config(
         val idProp: String
 )
 
-data class TreeConfig(val id: String = "tree"): Config(path = "D:/20230113.geojson", idProp = "ortsteil")
+data class TreeConfig(val id: String = "tree"): Config(path = "D:/20230120.geojson", idProp = "ortsteil")
 data class DistrictConfig(val id: String = "district"): Config(path = "D:\\git\\opendata-leipzig-playground\\docs\\ortsteile.json", idProp = "Name")
 
 fun main() {
