3c91346eae58bf0730a043c96ddec19275e5f8db
Jörg Reichert <de.abg.reichert.joerg@googlemail.com>
2023-02-17 22:57:43 +0100

    add refreshVersions

--- GeojsonSplitter/build.gradle.kts
+++ GeojsonSplitter/build.gradle.kts
@@ -4,7 +4,7 @@ version = "0.1-SNAPSHOT"
 plugins {
     application
     idea
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm")
 }
 
 java {
@@ -17,14 +17,14 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-    implementation("com.fasterxml.jackson.core:jackson-core:2.13.3")
-    implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.3")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
-    implementation("org.eclipse.jgit:org.eclipse.jgit:6.2.0.202206071550-r")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:_")
+    implementation("com.fasterxml.jackson.core:jackson-core:_")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:_")
+    implementation("com.fasterxml.jackson.core:jackson-databind:_")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:_")
+    implementation("org.eclipse.jgit:org.eclipse.jgit:_")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:_")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:_")
     implementation(kotlin("stdlib"))
 }
 
--- GeojsonSplitter/settings.gradle.kts
+++ GeojsonSplitter/settings.gradle.kts
@@ -1,1 +1,6 @@
 rootProject.buildFileName = "build.gradle.kts"
+
+plugins {
+    // See https://jmfayard.github.io/refreshVersions
+    id("de.fayard.refreshVersions") version "0.51.0"
+}
