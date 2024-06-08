7364213b618389b64f35cba468ca4cc17549694b
Jörg Reichert <de.abg.reichert.joerg@googlemail.com>
2024-06-08 21:24:58 +0200

    update 2024-06-07

--- GeojsonSplitter/build.gradle.kts
+++ GeojsonSplitter/build.gradle.kts
@@ -8,8 +8,8 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_19
+    targetCompatibility = JavaVersion.VERSION_19
 }
 
 repositories {
